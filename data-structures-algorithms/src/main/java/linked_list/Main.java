a54e42397bd518bc8506451762da0764a64286f9
Arsen Baktiyarov <devbaktiyarov@gmail.com>
2022-01-01 15:39:14 +0600

    Insertion method in singly linked list

--- data-structures-algorithms/src/main/java/linked_list/Main.java
+++ data-structures-algorithms/src/main/java/linked_list/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
         singlyLinkedList.createSinglyLinkedList(5);
+        singlyLinkedList.insertToLinkedList(3, 0);
+        singlyLinkedList.insertToLinkedList(2, 1);
         System.out.println(singlyLinkedList.size);
+        System.out.println(singlyLinkedList.head.value);
+        System.out.println(singlyLinkedList.head.next.value);
     }
 }
--- data-structures-algorithms/src/main/java/linked_list/SinglyLinkedList.java
+++ data-structures-algorithms/src/main/java/linked_list/SinglyLinkedList.java
@@ -16,4 +16,34 @@ public class SinglyLinkedList {
         return head;
     }
 
+    public void insertToLinkedList(int value,
+                                   int location) {
+        Node node = new Node(); // O(1)
+        node.value = value; // O(1)
+        if (head == null) {  // O(1)
+            createSinglyLinkedList(value); // O(1)
+        } else if (location == 0) { // O(1)
+            node.next = node; // O(1)
+            head = node; // O(1)
+        } else if (location >= size){  // O(1)
+            node.next = null;  // O(1)
+            tail.next = node; // O(1)
+            tail = node; // O(1)
+        } else {
+            Node tempNode = head;  // O(1)
+            int index = 0;  // O(1)
+            while (index < location - 1) { // O(N)
+                tempNode = tempNode.next; // O(1)
+                index++; // O(1)
+            }
+            Node nextNode = tempNode.next; // O(1)
+            tempNode.next = node; // O(1)
+            node.next = nextNode; // O(1)
+        }
+        size++; // O(1)
+    }
+
+
+
+
 }
