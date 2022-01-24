4b7a0999a2108a3ccb4aa467e47aebc80714453e
Arsen Baktiyarov <devbaktiyarov@gmail.com>
2022-01-24 19:08:56 +0600

    Create insertion sorting algorithm

---
+++ data-structures-algorithms/src/main/java/sorting_algorithm/InsertionSort.java
@@ -0,0 +1,15 @@
+package sorting_algorithm;
+
+public class InsertionSort {
+    public static void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while(j > 0 && arr[j - 1] > temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            } 
+            arr[j] = temp;
+        }
+    }
+}
